9705b7a86f604e49e9d9c125dbaeb300dfba67cd
Miguel Castillo <miguel.castillo1901@alumnos.ubiobio.cl>
2023-10-30 00:16:43 -0300

    funciones de notificaciones agregadas

--- backend/src/services/notification.service.js
+++ backend/src/services/notification.service.js
@@ -4,10 +4,16 @@ async function notificationNewBenefit(benefit) {
     try {
         const users = getUsers();
         users.forEach(user => {
-        if (user.roles.name === "user") {
-            /* Aqui va todo lo del correo*/
-        }
-    });
+            if (user.roles.name === "user") {
+                const mailOptions = {
+                    to_name: user.firstName,
+                    to_mail: user.email,
+                    subject: "Nuevo beneficio",
+                    message: `Hola ${user.firstName} ${user.lastName}, desde hoy tienes disponible el nuevo beneficio ${benefit.name}`,
+                };
+                /* Aqui va todo lo del correo*/
+            }
+        });
     } catch (error) {
         handleError(error, "notification.service -> notificationNewBenefit");
     }
@@ -15,14 +21,15 @@ async function notificationNewBenefit(benefit) {
 
 async function notificationChangeStatus(user) {
     try {
-        const users = getUsers();
-        users.forEach(user => {
-        if (user.roles.name === "user") {
-            /* Aqui va todo lo del correo*/
-        }
-    });
+        const mailOptions = {
+            to_name: user.firstName,
+            to_mail: user.email,
+            subject: "Estado de la solicitud",
+            message: `Hola ${user.firstName} ${user.lastName}, tu solicitud ha cambiado al estado ${user.applicationStatus}`,
+        };
+        /* Aqui va todo lo del correo*/
     } catch (error) {
-        handleError(error, "notification.service -> notificationNewBenefit");
+        handleError(error, "notification.service -> notificationChangeStatus");
     }
 }
 
