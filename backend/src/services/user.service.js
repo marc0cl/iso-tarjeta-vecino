7d4ea8897fd7d9a4761b6c9bc10b91213d7bd775
Marco <63360454+marc0cl@users.noreply.github.com>
2023-12-18 23:02:33 -0300

    Added logic

--- backend/src/services/user.service.js
+++ backend/src/services/user.service.js
@@ -166,41 +166,66 @@ async function getUserByEmail(email) {
 }
 
 /**
- * Actualiza un usuario por su id en la base de datos
- * @param {string} id Id del usuario
- * @param {Object} user Objeto de usuario
- * @returns {Promise} Promesa con el objeto de usuario actualizado
+
+ Actualiza un usuario por su id en la base de datos
+ @param {string} id Id del usuario
+ @param {Object} user Objeto de usuario
+ @returns {Promise} Promesa con el objeto de usuario actualizado
  */
 async function updateUserById(id, user) {
   try {
     const userFound = await User.findById(id);
     if (!userFound) return [null, "El usuario no existe"];
 
-    const { rut, email, password, newPassword, roles } = user;
-
-    const matchPassword = await User.comparePassword(
+    // Desestructura los campos necesarios de 'user', incluyendo los nuevos campos
+    const {
+      rut,
+      email,
       password,
-      userFound.password,
-    );
+      newPassword,
+      roles,
+      firstName,
+      lastName,
+      gender,
+      location,
+      residenceCertificate,
+      userType,
+      documentImage,
+      applicationStatus
+    } = user;
 
+
+    const matchPassword = await User.comparePassword(password, userFound.password);
     if (!matchPassword) {
       return [null, "La contraseña no coincide"];
     }
 
-    const rolesFound = await Role.find({ name: { $in: roles } });
-    if (rolesFound.length === 0) return [null, "El rol no existe"];
+    // Encuentra los roles si se han proporcionado
+    let myRole = userFound.roles;
+    if (roles) {
+      const rolesFound = await Role.find({ name: { $in: roles } });
+      if (rolesFound.length === 0) return [null, "El rol no existe"];
+      myRole = rolesFound.map((role) => role._id);
+    }
 
-    const myRole = rolesFound.map((role) => role._id);
-
+    // Actualiza el usuario
     const userUpdated = await User.findByIdAndUpdate(
-      id,
-      {
-        rut,
-        email,
-        password: await User.encryptPassword(newPassword || password),
-        roles: myRole,
-      },
-      { new: true },
+        id,
+        {
+          rut,
+          email,
+          password: newPassword ? await User.encryptPassword(newPassword) : userFound.password,
+          roles: myRole,
+          firstName,
+          lastName,
+          gender,
+          location,
+          residenceCertificate,
+          userType,
+          documentImage,
+          applicationStatus
+        },
+        { new: true }
     );
 
     return [userUpdated, null];
